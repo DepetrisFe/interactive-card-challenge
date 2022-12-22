26f1bca7f023054f0493cffa442ae192fc3c3525
Federico <depetrisfe@gmail.com>
2022-12-22 17:32:21 +0100

    card images displayed and positioned

--- src/mainPage/index.tsx
+++ src/mainPage/index.tsx
@@ -1,11 +1,34 @@
-import React from "react";
 import { useStyles } from "./styles";
-import { Grid } from "@mui/material";
+import { Box, Grid } from "@mui/material";
+import BgCardFront from "../assets/bg-card-front.png";
+import BgCardBack from "../assets/bg-card-back.png";
 
 const MainPage = () => {
   const classes = useStyles();
 
-  return <Grid className={classes.root}>hola</Grid>;
+  return (
+    <Grid container className={classes.root}>
+      <Grid item xs={6} className={classes.cardSection}>
+        <Box className={classes.cardContainer}>
+          <Box className={classes.cardFrontContainer}>
+            <img
+              src={BgCardFront}
+              alt="card front background"
+              className={classes.cardFront}
+            />
+          </Box>
+          <Box className={classes.cardBackContainer}>
+            <img
+              src={BgCardBack}
+              alt="card back background"
+              className={classes.cardBack}
+            />
+          </Box>
+        </Box>
+      </Grid>
+      <Grid item xs={6} className={classes.formSection}></Grid>
+    </Grid>
+  );
 };
 
 export default MainPage;
--- src/mainPage/styles.tsx
+++ src/mainPage/styles.tsx
@@ -3,9 +3,34 @@ import BgDesktop from "../assets/bg-main-desktop.png";
 
 export const useStyles = makeStyles({
   root: {
+    display: "flex",
     height: "100vh",
     backgroundImage: `url(${BgDesktop})`,
     backgroundRepeat: "no-repeat",
     backgroundSize: "35% 100%",
+  },
+  cardSection: {
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    border: "1px solid red",
+  },
+  cardContainer: {
+    display: "grid",
+    width: "100%",
+    gridRowGap: "30px",
+  },
+  cardFrontContainer: {
+    display: "flex",
+    justifyContent: "center",
+  },
+  cardBackContainer: {
+    display: "flex",
+    justifyContent: "flex-end",
+  },
+  cardFront: {},
+  cardBack: {},
+  formSection: {
+    border: "1px solid red",
   },
 });
